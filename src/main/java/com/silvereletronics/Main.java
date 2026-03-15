a3d1724f8b94026e8d0ea0fa03185e851f4e3f5a
SilverDeveloper <52933126+silver-the-dev@users.noreply.github.com>
2026-03-15 19:16:06 -0300

    Read operation ready

--- src/main/java/com/silvereletronics/Main.java
+++ src/main/java/com/silvereletronics/Main.java
@@ -1,11 +1,9 @@
 package com.silvereletronics;
 
-import com.silvereletronics.db.DBOperations;
 import com.silvereletronics.utils.Menu;
 
 public class Main {
     public static void main(String[] args) {
-        Menu.listar();
-        System.out.println("Foi");
+        Menu.listarValores();
     }
 }

f3488bd229b805c731990e297bee7c020fb864d4
SilverDeveloper <52933126+silver-the-dev@users.noreply.github.com>
2026-03-17 20:06:43 -0300

    CRUD operations done

--- src/main/java/com/silvereletronics/db/DBOperations.java
+++ src/main/java/com/silvereletronics/db/DBOperations.java
@@ -6,16 +6,17 @@ import java.sql.*;
 import java.util.*;
 
 public class DBOperations {
-    public static void create(Componente comp){
+    public static int create(Componente comp){
         String sql = "INSERT INTO componentes (nome, quantidade) VALUES (?, ?)";
         try(Connection conn = DBConnect.getConnection();
             PreparedStatement stmt = conn.prepareStatement(sql)){
             stmt.setString(1, comp.getNome());
             stmt.setInt(2, comp.getQuantidade());
-            stmt.executeUpdate();
+            return stmt.executeUpdate();
         } catch (SQLException e){
             System.out.println("Erro ao adicionar componente: " + e.getMessage());
         }
+        return 0;
     }
 
     public static List<Componente> read() {
@@ -36,10 +37,41 @@ public class DBOperations {
         return componentes;
     }
 
-    public static void update(){
-
+    public static void update(int id, String nome, int quantidade) {
+        String sql = "UPDATE componentes SET nome = ?, quantidade = ? WHERE id = ?";
+        try(Connection conn = DBConnect.getConnection()){
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, nome);
+            stmt.setInt(2, quantidade);
+            stmt.setInt(3, id);
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizar componente no banco de dados: " + e.getMessage());
+        }
     }
-    public static void delete(){
+    public static void delete(int id){
+        String sql = "DELETE FROM componentes WHERE id = ?";
+        try(Connection conn = DBConnect.getConnection()){
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e){
+            System.out.println("ID não existe " + e.getMessage());
+        }
+    }
 
+    public static int getMaxID(){
+        int id;
+        try(Connection conn = DBConnect.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("SELECT COUNT(id) AS total FROM componentes");
+            ResultSet rs = stmt.executeQuery()){
+            if(rs.next()){
+                id = rs.getInt("total");
+                return id;
+            }
+        }catch (SQLException e){
+            System.out.println("Erro de conexão" + e.getMessage());
+        }
+         return 0;
     }
 }
--- src/main/java/com/silvereletronics/utils/Menu.java
+++ src/main/java/com/silvereletronics/utils/Menu.java
@@ -3,6 +3,7 @@ package com.silvereletronics.utils;
 import com.silvereletronics.Componente;
 import com.silvereletronics.db.DBOperations;
 
+import javax.sound.midi.Soundbank;
 import java.util.List;
 
 public class Menu {
@@ -22,11 +23,13 @@ public class Menu {
         }
     }
 
-    public static void atualizar() {
-        DBOperations.update();
+    public static void atualizar(int id, String nome, int quantidade) {
+        DBOperations.update(id, nome, quantidade);
     }
-    public static void deletar() {
-        DBOperations.delete();
+    public static void deletar(int id) {
+
+        DBOperations.delete(id);
+
     }
 
     public static void menu() {
@@ -34,19 +37,33 @@ public class Menu {
         System.out.println("Selecione uma opção: ");
         System.out.println("1 - Listar.");
         System.out.println("2 - Inserir.");
-        //System.out.println("3 - Atualizar.");
-        //System.out.println("4 - Deletar.");
+        System.out.println("3 - Atualizar.");
+        System.out.println("4 - Deletar.");
 
         System.out.println("Opção: ");
-        int opcao = DataInput.IntegerInput(1,2);
+        int opcao = DataInput.IntegerInput(1,4);
         if (opcao == 1) {
             listarValores();
         } else if (opcao == 2) {
-            inserir("Resistor genérico", 123);
+            System.out.println("Digite o nome do componente: ");
+            String nome = DataInput.TextInput();
+            System.out.println("Digite a quantidade de componentes disponíveis");
+            int quantidade = DataInput.IntegerInput();
+            inserir(nome, quantidade);
+            System.out.println("Item inserido com sucesso");
         } else if (opcao == 3) {
-            atualizar();
+            System.out.println("Digite o novo nome: ");
+            String nome = DataInput.TextInput();
+            System.out.println("Digite a nova quantidade: ");
+            int quantidade = DataInput.IntegerInput();
+            System.out.println("Digite o ID do item à aplicar estas modificações");
+            int id = DataInput.IntegerInput(1, DBOperations.getMaxID());
+            atualizar(id, nome, quantidade);
+            System.out.println("Item atualizado com sucesso");
         } else if (opcao == 4) {
-            deletar();
+            System.out.println("Digite o ID do produto a remover: ");
+            int id = DataInput.IntegerInput();
+            deletar(id);
         } else {
             System.out.println("Opção inválida.");
         }
